94eef9fc91369423a5c60db22e12787431122326
czechowskijakub <kubaczech911@gmail.com>
2025-07-17 19:28:04 +0200

    minor changes, positions window refreshes after addition

--- DishPositions.py
+++ DishPositions.py
@@ -11,31 +11,7 @@ class PositionsWindow:
         self.dish_tab = Toplevel(parent)
         self.dish_tab.title(dish.name)
 
-        lbl = Label(self.dish_tab, 
-            text = f"{dish.name} Positions:", 
-            anchor = "center",
-            width = 30,
-            height = 2,
-            font = ("PT Sans", 14, "bold")
-        )
-        lbl.grid()
-    
-        for pos, price in dish.Positions_list.items():
-            dish_btn = Button(self.dish_tab, 
-                    text = f"{pos}: {price}zl", 
-                    command = lambda p=pos, pr=price: self.bill_manager.add_position(p, pr)
-            )               
-            dish_btn.grid(padx=5, pady=2)
-            
-        self.name_entry = Entry(self.dish_tab, width=10)
-        self.name_entry.grid(pady=5)
-        self.price_entry = Entry(self.dish_tab, width=10)
-        self.price_entry.grid(pady=5)
-        
-        self.accept_btn = Button(self.dish_tab, text="Add position", command=self.add_position)
-        self.accept_btn.grid()
-        
-        self.price_entry.bind("<Return>", self.add_position)
+        self.display()
             
     def add_position(self, event=None):
         name = str(self.name_entry.get()).capitalize()
@@ -54,8 +30,41 @@ class PositionsWindow:
         self.name_entry.delete(0, END)
         self.price_entry.delete(0, END)
         
-        self.dish_tab.destroy()
-        PositionsWindow(self.parent, self.dish_manager, self.bill_manager, self.dish)
-        for key, val in self.dish.Positions_list.items():
-            print(key, val)
+        self.update()
+            
+    def update(self):
+        for widget in self.dish_tab.winfo_children():
+            widget.destroy()      
+
+        self.display()
+        
+    def display(self):
+        lbl = Label(self.dish_tab, 
+            text = f"{self.dish.name} Positions:", 
+            anchor = "center",
+            width = 30,
+            height = 2,
+            font = ("PT Sans", 14, "bold")
+        )
+        lbl.grid()
+    
+        for pos, price in self.dish.Positions_list.items():
+            dish_btn = Button(self.dish_tab, 
+                    text = f"{pos}: {price}zl", 
+                    command = lambda p=pos, pr=price: self.bill_manager.add_position(p, pr)
+            )               
+            dish_btn.grid(padx=5, pady=2)
+            
+        self.name_entry = Entry(self.dish_tab, width=10)
+        self.name_entry.grid(pady=5)
+        self.price_entry = Entry(self.dish_tab, width=10)
+        self.price_entry.grid(pady=5)
+        
+        self.accept_btn = Button(self.dish_tab, text="Add position", command=self.add_position)
+        self.accept_btn.grid()
+        
+        self.price_entry.bind("<Return>", self.add_position)
+    
+    def remove_position(self):
+        pass
         
